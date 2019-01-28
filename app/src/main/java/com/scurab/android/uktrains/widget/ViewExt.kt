605ec9b415854ff366e29d58a3f48336b64e8786
Jiri Bruchanov <jbruchanov@gmail.com>
2019-01-28 23:33:48 +0000

    Improve station picker

--- app/src/main/java/com/scurab/android/uktrains/widget/ViewExt.kt
+++ app/src/main/java/com/scurab/android/uktrains/widget/ViewExt.kt
@@ -1,21 +1,19 @@
 package com.scurab.android.uktrains.widget
 
+import android.graphics.Color
 import android.graphics.Typeface
 import android.widget.TextView
 import android.text.Spannable
 import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
 
 
 fun TextView.textTextWithBoldLetters(text: String, boldIndexes: IntArray) {
     val str = SpannableStringBuilder(text)
     boldIndexes.forEach {
-        str.setSpan(
-            StyleSpan(Typeface.BOLD),
-            it,
-            it+1,
-            Spannable.SPAN_INCLUSIVE_INCLUSIVE
-        )
+        str.setSpan(StyleSpan(Typeface.BOLD), it, it+1, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        str.setSpan(ForegroundColorSpan(Color.WHITE), it, it+1, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
     }
     setText(str)
 }
